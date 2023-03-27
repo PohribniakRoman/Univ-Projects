c9f53801d4e13f8ea4e09df669c53ec879151697
PohribniakRoman <79878288+PohribniakRoman@users.noreply.github.com>
2023-03-27 23:21:43 +0300

    rewrite with insert method

--- dz3/main.cpp
+++ dz3/main.cpp
@@ -6,10 +6,10 @@ using namespace std;
 
 int main(){
     vector<int> uniq = {1,2,3,4,5};
-    vector<int> append = {1,3,5,6,12,123};
+    vector<int> append = {1,3,-2,1231313,5,6,12,123};
     for (auto it = append.begin(); it != append.end(); it++) {
         if(find(uniq.begin(),uniq.end(),*it) == uniq.end()){
-            uniq.push_back(*it);
+            uniq.insert(uniq.end(),*it);
         }
     }
     for (int i = 0; i < uniq.size(); ++i) {
