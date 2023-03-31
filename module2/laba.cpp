43280ceda79c55cc2288d09fd77a5e5f1ce6234b
PohribniakRoman <79878288+PohribniakRoman@users.noreply.github.com>
2023-04-01 01:47:05 +0300

    updated

--- module2/laba.cpp
+++ module2/laba.cpp
@@ -8,28 +8,51 @@ struct Dis{
     string name;
     int id;
 };
-
-struct Subject {
-    Dis discipline;
-    int grade;
-};
-
 class Student{
 private:
     string name;
     string surname;
+    const int SUBJECT_SIZE = 55;
     int discipline_count = 0;
 public:
+    struct Subject {
+        Dis discipline;
+        int grade;
+    };
+    Subject * subjects = new Subject[SUBJECT_SIZE];
     Student();
     Student(string name, string surname);
-    Subject * subjects = new Subject[55];
     bool operator +(Subject subjectToAdd);
-    bool operator < (Student compareStudent);
-    void operator >>(ofstream &outStream);
+    bool operator < (Student &compareStudent){
+        double thisAverage = 0;
+        double compareAverage = 0;
+        for (int i = 0; i < this->discipline_count; i++) {
+            thisAverage += this->subjects[i].grade;
+        }
+        thisAverage /= this->discipline_count;
+        for (int x = 0; x < compareStudent.discipline_count; x++) {
+            compareAverage += compareStudent.subjects[x].grade;
+        }
+        compareAverage /= compareStudent.discipline_count;
+        if (thisAverage < compareAverage) {
+            return true;
+        }
+        return false;
+    };
+    void operator >>(ostream &output){
+        output<< this->name <<" "<< this->surname <<"\n";
+        for (int i = 0; i < this->discipline_count; ++i) {
+            output<<"Discipline: "
+                << this->subjects[i].discipline.name
+                << " | id - " <<  this->subjects[i].discipline.id
+                <<";\n" <<"Grade: " << this->subjects[i].grade << "\n";
+        }
+    };
     void setName(string name);
     void setSurname(string name);
     ~Student();
 };
+
 Student::Student(){}
 Student::Student(string name, string surname) {
     this->name = name;
@@ -40,41 +63,11 @@ bool Student::operator +(Subject subjectToAdd) {
     if (subjectToAdd.grade >= 0 && subjectToAdd.grade <= 100){
         this->subjects[discipline_count] = subjectToAdd;
         this->discipline_count++;
-        cout<<"Successfully added\n";
         return true;
     }
     cout<<"Failed to add\n";
     return false;
 }
-
-bool Student::operator < (Student compareStudent){
-    double thisAverage = 0;
-    double compareAverage = 0;
-    for (int i = 0; i < this->discipline_count; i++) {
-        thisAverage += this->subjects[i].grade;
-    }
-    thisAverage /= this->discipline_count;
-    for (int x = 0; x < compareStudent.discipline_count; x++) {
-        compareAverage += compareStudent.subjects[x].grade;
-    }
-    compareAverage /= compareStudent.discipline_count;
-    if (thisAverage < compareAverage) {
-        return true;
-    }
-    return false;
-}
-
-void Student::operator >>(ofstream &outStream){
-    string fileName = this->name+this->surname+".txt";
-    outStream.open(fileName);
-    outStream<<"Name: "<< this->name << " " << this->surname<<";\n";
-    for (int i = 0; i < this->discipline_count; ++i) {
-        outStream<<"Discipline: "
-                 << this->subjects[i].discipline.name
-                 << " | id - " <<  this->subjects[i].discipline.id
-                 <<";\n" <<"Grade: " << this->subjects[i].grade << "\n";
-    }
-};
 
 void Student::setName(string name){
     this->name = name;
@@ -83,7 +76,6 @@ void Student::setName(string name){
 void Student::setSurname(string surname){
     this->surname = surname;
 }
-
 
 Student::~Student(){
     delete [] this->subjects;
@@ -114,9 +106,9 @@ void fillStudent(Student &student, ifstream &dataStream){
             };
             arrString[2] = data;
             Dis discipline;
+            Student::Subject newSubject;
             discipline.name = arrString[0];
             discipline.id = stoi(arrString[1]);
-            Subject newSubject;
             newSubject.discipline = discipline;
             newSubject.grade = stoi(arrString[2]);
             student + newSubject;
@@ -128,11 +120,14 @@ void fillStudent(Student &student, ifstream &dataStream){
 }
 
 int main() {
-    Student Roman;
+    Student Max;
     ifstream dataStream("index.txt");
-    fillStudent(Roman,dataStream);
+    fillStudent(Max,dataStream);
     ofstream outPutFile;
-    Roman>>outPutFile;
-
+    Max>>cout;
     dataStream.close();
+    ifstream data2Stream("roman.txt");
+    Student Roman;
+    fillStudent(Roman,data2Stream);
+    data2Stream.close();
 }
